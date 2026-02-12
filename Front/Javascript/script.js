36461db9fe2dc8bcbd549817c140138d95fffcae
Ruiz-z <mauroruiz0404@gmail.com>
2026-02-12 17:28:56 -0600

    Se agregan nuevas funcionalidades para el dashboard para el modelo y se termina todas las funcionalidades

--- Front/Javascript/script.js
+++ Front/Javascript/script.js
@@ -3,5 +3,5 @@ const sidebar = document.getElementById('sidebar')
 
 menu.addEventListener('click',()=>{
     sidebar.classList.toggle('menu-toggle');
-    menu.classlist.toggle('menu-toggle');
+    menu.classList.toggle('menu-toggle');
 });
